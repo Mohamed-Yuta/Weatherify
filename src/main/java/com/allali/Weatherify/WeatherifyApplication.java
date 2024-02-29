8a350ea478741b379d1e90b3202471a70dbc736e
Mohamed-Yuta <allali.mohamedamine22@gmail.com>
2024-02-29 13:26:52 +0100

    first commit

---
+++ src/main/java/com/allali/Weatherify/WeatherifyApplication.java
@@ -0,0 +1,13 @@
+package com.allali.Weatherify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allali/Weatherify/WeatherifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allali.Weatherify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
