5684c0136f859dea5341a067d205d27b83a3c382
Mohamed-Yuta <allali.mohamedamine22@gmail.com>
2024-03-01 18:49:38 +0100

    new message

--- src/main/java/com/allali/Weatherify/services/WeatherService.java
+++ src/main/java/com/allali/Weatherify/services/WeatherService.java
@@ -13,17 +13,36 @@ public class WeatherService {
     private final String key ="f345577cc2f799878b461c92b791477e";
     private final String url = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
     private final String geoUrl="http://api.openweathermap.org/geo/1.0/direct?q={cityName}&limit=5&appid={API key}";
+    private final String geoUrl2="http://api.openweathermap.org/geo/1.0/zip?zip={zip code}&appid={API key}";
     public Coord getCoord(String cityName){
         return webClient.build()
                 .get()
-                .uri(geoUrl,cityName,key)
+                .uri(geoUrl2,cityName,key)
                 .retrieve()
                 .bodyToFlux(Coord.class) // Note the change to bodyToFlux
                 .next() // Get the first item from the Flux
                 .block(); // Block to wait for the response
     }
+    public Coord getCoordByZipCode(String zipCode){
+        return webClient.build()
+                .get()
+                .uri(geoUrl,zipCode,key)
+                .retrieve()
+                .bodyToFlux(Coord.class)
+                .next()
+                .block();
+
+    }
     public WeatherData getWeatherData(String cityName) {
         Coord coord = getCoord(cityName);
+        return webClient.build().get()
+                .uri(url,coord.getLat(),coord.getLon(),key)
+                .retrieve()
+                .bodyToMono(WeatherData.class)
+                .block();
+    }
+    public WeatherData getWeatherByZipCode(String zipCode){
+        Coord coord = getCoordByZipCode(zipCode);
         return webClient.build().get()
                 .uri(url,coord.getLat(),coord.getLon(),key)
                 .retrieve()
