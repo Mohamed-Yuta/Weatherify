d37e0188bcfc57d17a9999690151256ead4c82fc
Mohamed-Yuta <allali.mohamedamine22@gmail.com>
2024-03-02 16:56:53 +0100

    adding chatResponse

--- src/main/java/com/allali/Weatherify/dtos/chatGpt/ChatResponse.java
+++ src/main/java/com/allali/Weatherify/dtos/chatGpt/ChatResponse.java
@@ -1,2 +1,24 @@
-package com.allali.Weatherify.dtos.chatGpt;public class ChatResponse {
+package com.allali.Weatherify.dtos.chatGpt;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatResponse {
+    private List<Choice> choices;
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Choice {
+
+        private int index;
+        private Message message;
+
+    }
+
 }
